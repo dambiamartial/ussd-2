df82a95d183b6dc777d096679ea9606d1c336c5f
Martial DAMBIA <dambiamartial@gmail.com>
2023-05-05 19:52:33 +0100

    test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,40 +9,40 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.post("/ussd", (request, resp)=> {
-  const {
-    phoneNumber,
-    sessionId,
-    serviceCode,
-    text
-  } = request.body;
+	const {
+		phoneNumber,
+		sessionId,
+		serviceCode,
+		text
+	} = request.body;
 
-  let response = "";
+	let response = "";
 
-  switch (text) {
-  case "":
-    response = "CON What would you like to check? <br/> 1. My Account <br/> 2. Service code <br/> 3. Session id";
-    break;
-  case "1":
-    response ="CON Choose account information you want to view <br/> 1. Account Number <br/> 2. Account balancet";
-    break;
-  case "2":
-    response = "END Service code is "+serviceCode;
-    break;
-  case "3":
-    response = "END Session id is "+sessionId;
-    break;
-  case "1*1":
-    response = "END your phone number is "+phoneNumber;
-    break;
-  case "1*2":
-    response = "END your balance is "+"10000";
-    break;
-  default:
-    response = "END Session end";
-  }
+	switch (text) {
+	case "":
+		response = "CON What would you like to check? <br/> 1. My Account <br/> 2. Service code <br/> 3. Session id";
+		break;
+	case "1":
+		response ="CON Choose account information you want to view <br/> 1. Account Number <br/> 2. Account balancet";
+		break;
+	case "2":
+		response = "END Service code is "+serviceCode;
+		break;
+	case "3":
+		response = "END Session id is "+sessionId;
+		break;
+	case "1*1":
+		response = "END your phone number is "+phoneNumber;
+		break;
+	case "1*2":
+		response = "END your balance is "+"10000";
+		break;
+	default:
+		response = "END Session end";
+	}
 
-  resp.set("Content-Type: text/plain");
-  resp.send(response);
+	resp.set("Content-Type: text/plain");
+	resp.send(response);
 });
 
 exports.ussd = functions.https.onRequest(app);
